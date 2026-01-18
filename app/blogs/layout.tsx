4921bc1583e16a3e8403db40e2366ed63086f3b7
mohamedmaher <mohamedmaher.mm330@gmail.com>
2026-01-18 15:26:30 +0200

    add mainfest file and app metadata

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,9 +1,16 @@
+import Head from "next/head";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="container mx-auto py-10 px-10 max-w-6xl">{children}</div>
+    <div className="container mx-auto py-10 px-10 max-w-6xl">
+      <Head>
+        <meta name="apple-mobile-web-app-title" content="DevHub" />
+      </Head>
+      {children}
+    </div>
   );
 }
